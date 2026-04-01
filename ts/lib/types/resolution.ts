ff9046d12c788ca2000ffdf6d8d47ef35f5c1e7d
Matthias Van Parijs <matvp91@gmail.com>
2026-04-01 22:51:23 +0200

    migrate(types): convert resolution externs to TypeScript interface

---
+++ ts/lib/types/resolution.ts
@@ -0,0 +1,11 @@
+/*! @license
+ * Shaka Player
+ * Copyright 2016 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/** Width and height in pixels. */
+export interface Resolution {
+  width: number;
+  height: number;
+}
